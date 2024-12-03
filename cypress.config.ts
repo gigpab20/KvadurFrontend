09b09a658b06d47842e6aa34c96c144e244412e0
gigpab20 <gigpab20@htl-kaindorf.at>
2024-12-03 11:39:58 +0100

    changed url again

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://0.0.0.0:46081', // Passen Sie dies an Ihre tatsächliche URL an
+    baseUrl: 'http://mirfac.uberspace.de:46081', // Passen Sie dies an Ihre tatsächliche URL an
   },
 });
