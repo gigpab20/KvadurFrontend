de5b4b26cc1f35375534308c9a4629412a4e5e4d
Jonas brunner <brujob20@htl-kaindorf.at>
2024-12-10 14:33:43 +0100

    einzelne Testaufspaltung + relativen Pfad

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://mirfac.uberspace.de:46081', // Passen Sie dies an Ihre tatsächliche URL an
+    baseUrl: 'http://mirfac.uberspace.de:46081', // Haupturl
   },
 });
