53d7b1f3e65b14b28a8e6a615b8e1fcab80aa38b
gigpab20 <gigpab20@htl-kaindorf.at>
2025-01-14 13:53:34 +0100

    changed action

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
     const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
-        fetch('http://mirfac.uberspace.de/products')
+        fetch('http://localhost:46081/products')
             .then(response => response.json())
             .then(data => {
                 setProducts(data.products);
